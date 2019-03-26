0495c1c4e571860f92c84df3124a12f50cf241b2
David De Sousa <Sankhara@imac.local>
2019-03-26 10:15:00 -0700

    Updates

--- precache-manifest.cbb1f8351575297ed5797973d946f8ca.js
+++ precache-manifest.cbb1f8351575297ed5797973d946f8ca.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/23imgs/static/js/runtime~main.0096b50e.js"
   },
   {
-    "revision": "e178f47e8ebce29b64ef",
-    "url": "/23imgs/static/js/main.23f6cedc.chunk.js"
+    "revision": "8bb5679fb7c369f1210b",
+    "url": "/23imgs/static/js/main.a5a2b04a.chunk.js"
   },
   {
     "revision": "fc7cb5e338c0dc5b0a56",
     "url": "/23imgs/static/js/2.f4a371d1.chunk.js"
   },
   {
-    "revision": "e178f47e8ebce29b64ef",
+    "revision": "8bb5679fb7c369f1210b",
     "url": "/23imgs/static/css/main.e7121194.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/23imgs/static/css/2.295d9f09.chunk.css"
   },
   {
-    "revision": "9cdcc636454db71a7739c7bd8e32aba1",
+    "revision": "e605703a51d83ca10925079343d16884",
     "url": "/23imgs/index.html"
   }
 ];
